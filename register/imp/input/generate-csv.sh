9f90c3b7b7d1f62f22cf310da71b23c00fad7579
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2017-12-30 16:24:23 +0100

    New import plus some fixes.

--- register/imp/input/generate-csv.sh
+++ register/imp/input/generate-csv.sh
@@ -1,8 +1,9 @@
-xlsx2csv -n "Sheet1" -i -d "tab" lijstenB.xlsx lijstenB.csv
-xlsx2csv -n "Sheet1" -i -d "tab" lijstenI.xlsx lijstenI.csv
-xlsx2csv -n "Sheet1" -i -d "tab" recensenten.xlsx recensenten.csv
-xlsx2csv -n "Sheet1" -i -d "tab" algemeen.xlsx algemeen.csv
-xlsx2csv -n "Sheet1" -i -d "tab" rubrieken.xlsx rubrieken.csv
+rm -f *.csv 
+xlsx2csv -s 2 -i -d "tab" lijstenB.xlsx lijstenB.csv
+xlsx2csv -s 1 -i -d "tab" lijstenI.xlsx lijstenI.csv
+xlsx2csv -s 1 -i -d "tab" recensenten.xlsx recensenten.csv
+xlsx2csv -s 3 -i -d "tab" algemeen.xlsx algemeen.csv
+xlsx2csv -s 1 -i -d "tab" rubrieken.xlsx rubrieken.csv
 sed -i '1d' lijstenB.csv
 sed -i '1d' lijstenI.csv
 sed -i '1d' algemeen.csv
